d24026bceca9f480a15c887500ad44fb59cac7eb
aakash14goplani <goplaniaakash14@gmail.com>
2019-10-14 02:09:00 +0530

    completed voting scenario

--- src/app/vote/vote.component.spec.ts
+++ src/app/vote/vote.component.spec.ts
@@ -1,25 +1,19 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { VoteComponent } from './vote.component';
 
 describe('VoteComponent', () => {
   let component: VoteComponent;
-  let fixture: ComponentFixture<VoteComponent>;
-
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ VoteComponent ]
-    })
-    .compileComponents();
-  }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(VoteComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    component = new VoteComponent();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should increment vote counter when upvoted', () => {
+    component.upVote();
+    expect(component.totalVotes).toBe(1);
+  });
+
+  it('should decrement vote counter when downvoted', () => {
+    component.downVote();
+    expect(component.totalVotes).toBe(0);
   });
 });
